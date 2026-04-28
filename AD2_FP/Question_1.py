e5f36e084fd9bd0004198349100dcd6df0f956d8
Nicolas Scarp <contatonicolasscarp@gmail.com>
2026-04-28 00:45:47 -0300

    Feature: Update Question_1.py

--- AD2_FP/Question_1.py
+++ AD2_FP/Question_1.py
@@ -32,11 +32,49 @@ while True:
         continue
 
 list2=[]
+vi = []
 
 while True:
     list2 = input("").split()
-    if len(list2) != N:
+    if len(list2) < N:
         print("Valores insuficientes!\nTente Novamente.")
+        continue
+    elif len(list2) > N:
+        print("Mais valores que o esperado!\nTente Novamente.")
+        continue
+    else:
+        for i in range(len(list2)):
+            vi.append(int(list2[i]))
+            break
+        break
+
+mini=0
+maxi=0
+
+for i in range(len(vi)):
+
+    if vi[i] < 1:
+        mini=mini+1
+        
+        continue
+    elif vi[i] > 100:
+        maxi=maxi+1
+        
         continue
     else:
         break
+    
+
+print(mini)
+print(maxi)
+
+
+
+print("Tempo não pode ser menor que 1!\nDigite novamente.")
+
+print("Tempo não pode ser maior que 100!\nDigite novamente.")            
+            
+
+
+
+    
